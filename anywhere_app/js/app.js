6cda64e6963b934f8c81384f65370fb2f9efb87f
erwin237 <mkevinewin@gmail.com>
2025-02-28 16:05:49 +0100

    writing a js function to compute the sum of numbers

---
+++ anywhere_app/js/app.js
@@ -0,0 +1,21 @@
+var nom = "kevin"
+console.log(nom)
+
+const age = [1, 13, 26,46]
+console.log(age)
+age[8] = 23
+console.log(age[8])
+
+function factoriel() {
+  console.log("Bonjour le monde")
+}
+
+function somme(a, b) {
+  return a + b
+}
+
+function sommeTrois(a, b, c) {
+  return somme(a, b) + c
+}
+
+console.log(sommeTrois(1,2,3))
