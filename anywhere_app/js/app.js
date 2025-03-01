013d0c97cb9a5f6dca354a795a0db513fd0e9ccb
erwin237 <mkevinewin@gmail.com>
2025-03-01 03:23:06 +0100

    write a javascript

--- anywhere_app/js/app.js
+++ anywhere_app/js/app.js
@@ -44,3 +44,48 @@ function factorielle (n) {
 }
 console.log(factorielle(5));
 
+
+
+
+function combinaison(n, p) {
+  if (p > n) {
+    throw new Error("p ne peut pas être supérieur à n.");
+  }
+
+  function factorielle(x) {
+    let resultat = 1;
+    for (let i = 2; i <= x; i++) {
+      resultat *= i;
+    }
+    return resultat;
+  }
+
+  let resultat = factorielle(n) / (factorielle(p) * factorielle(n - p));
+  console.log("La combinaison de {p} dans {n} est : {resultat}");
+}
+
+
+
+
+function estPremier(n) {
+  if (n <= 1) {
+    console.log("{n} n'est pas un nombre premier.");
+  } else if (n === 2) {
+    console.log("{n} est un nombre premier.");
+  } else {
+    let estPremier = true;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        estPremier = false;
+        break;
+      }
+    }
+    if (estPremier) {
+      console.log("{n} est un nombre premier.");
+    } else {
+      console.log(`${n} n'est pas un nombre premier.`);
+    }
+  }
+}
+cosole.log(estPremier(25));
+
