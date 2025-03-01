f88d687d8968d90ac9c9696626bf72465e484e34
amadiyawa <amadiyawa@yahoo.com>
2025-03-01 13:42:31 +0100

    fix functions inside app.js and add dom dependency

--- anywhere_app/js/app.js
+++ anywhere_app/js/app.js
@@ -1,91 +1,46 @@
-var nom = "kevin"
-console.log(nom)
+const { JSDOM } = require('jsdom');
+const { window } = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+const { document } = window;
 
-const age = [1, 13, 26,46]
-console.log(age)
-age[8] = 23
-console.log(age[8])
+let paragraphs = document.getElementsByTagName('p')
+console.log(paragraphs)
 
-function factoriel() {
-  console.log("Bonjour le monde")
-}
-
-function somme(a, b) {
-  return a + b
-}
-
-function sommeTrois(a, b, c) {
-  return somme(a, b) + c
-}
-
-console.log(sommeTrois(1,2,3))
-
-
-let student  = {
-  nom:"kevin",
-  age:17,
-  size:1.83,
-  room:"premiere ti",
-  location:"enia",
-};
-console.log(student)
-
-function factorielle (n) {
+function factorial(n) {
   if (n < 0) {
-    throw new error ("le parametre doit etre un entier naturel.");
+    return "not defined";
   } else if (n === 0 || n === 1) {
-
     return 1;
   } else {
-    return n *
-    factorielle(n - 1);
+    return n * factorial(n - 1);
   }
-
 }
-console.log(factorielle(5));
+console.log("The factorial of 5 is " + factorial(5));
 
-
-
-
-function combinaison(n, p) {
+function combining(p, n) {
   if (p > n) {
-    throw new Error("p ne peut pas être supérieur à n.");
-  }
-
-  function factorielle(x) {
-    let resultat = 1;
-    for (let i = 2; i <= x; i++) {
-      resultat *= i;
-    }
-    return resultat;
-  }
-
-  let resultat = factorielle(n) / (factorielle(p) * factorielle(n - p));
-  console.log("La combinaison de {p} dans {n} est : {resultat}");
-}
-
-
-
-
-function estPremier(n) {
-  if (n <= 1) {
-    console.log("{n} n'est pas un nombre premier.");
-  } else if (n === 2) {
-    console.log("{n} est un nombre premier.");
+    console.log("p cannot be greater than n");
   } else {
-    let estPremier = true;
-    for (let i = 2; i <= Math.sqrt(n); i++) {
+    console.log("p combining n is " + factorial(n) / (factorial(p) * factorial(n - p)));
+  }
+}
+combining(3, 6);
+
+function primeNumber(n) {
+  if (n < 2) {
+    return n + " is not a prime number";
+  } else {
+    let isPrime = true;
+    for (let i = 2; i <= n / 2; i++) {
       if (n % i === 0) {
-        estPremier = false;
+        isPrime = false;
         break;
       }
     }
-    if (estPremier) {
-      console.log("{n} est un nombre premier.");
+    if (isPrime) {
+      return n + " is a prime number";
     } else {
-      console.log(`${n} n'est pas un nombre premier.`);
+      return n + " is a not a prime number";
     }
   }
 }
-cosole.log(estPremier(25));
-
+console.log(primeNumber(13));
